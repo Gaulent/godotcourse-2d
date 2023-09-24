4d509237a7750dec64299ed3bfcda9d8b7c525dc
Gaulent <jose.saucod@gmail.com>
2023-09-24 14:42:32 +0200

    Incluidos GameManager, Customer y Package

--- DeliveryDriver/GameManager.cs
+++ DeliveryDriver/GameManager.cs
@@ -1,21 +1,11 @@
 using Godot;
 using System;
 
-public partial class GameManager : Node
+public partial class GameManager : Node2D
 {
-	public int Score = 0;
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+	private static int Score = 0;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-	
-	public void AddPoints()
+	public static void AddPoints()
 	{
 		Score++;
 		GD.Print(Score);
