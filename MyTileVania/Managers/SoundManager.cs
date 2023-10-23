63cbabd6e809a3d1c707d96fe1fcbd45007fb664
Gaulent <jose.saucod@gmail.com>
2023-10-24 01:22:45 +0200

    MyTileVania. Creacion de SingletonNode

--- MyTileVania/Managers/SoundManager.cs
+++ MyTileVania/Managers/SoundManager.cs
@@ -1,16 +1,8 @@
 using Godot;
 using System;
 
-public partial class SoundManager : Node
+public partial class SoundManager : SingletonNode<SoundManager>
 {
-	// Singleton Pattern
-	public static SoundManager Instance { get; private set; }
-
-	public override void _Ready()
-	{
-		Instance = this;
-	}
-
 	public void PlaySound(AudioStream audio)
 	{
 		AudioStreamPlayer player = new AudioStreamPlayer();
